f88309735a3dd65d872d0ca1ef6b1485a6860ce1
bellasimi <gksmf6611@gmail.com>
2022-10-08 13:15:56 +0900

    style: themeProvider 추가

---
+++ src/utils/theme.js
@@ -0,0 +1,20 @@
+const colors = {
+  mainBlue: "#73ABFF",
+  darkBlue: "#4991FD",
+  mainBlack: "#000000",
+  mainWhite: "#FFFFFF",
+  error: "#d32f2f",
+  success: "#4caf50",
+  fontBlue: "#1E67BC",
+  fontLight: "#A3A3A3",
+  borderBasic: "#CCCCCC",
+  borderLight: "#F1F1F1",
+  backgroundBasic: "#FFFFFF",
+  backgroundBlue: "#73ABFF",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
