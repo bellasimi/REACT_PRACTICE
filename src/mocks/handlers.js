0ea33d795b43e073d517c5af503e8f61e4ed73f7
bellasimi <gksmf6611@gmail.com>
2022-10-14 13:15:56 +0900

    feat: msw 추가

---
+++ src/mocks/handlers.js
@@ -0,0 +1,39 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.post("/login", (req, res, ctx) => {
+    sessionStorage.setItem("is-authenticated", "true");
+    return res(
+      ctx.status(200),
+      ctx.json({
+        accessToken: { token: "bearer token", expires: "2022-10-22" },
+        user: {
+          nickName: "신영",
+          id: "test@co.kr",
+        },
+      })
+    );
+  }),
+  rest.post("/signUp", (req, res, ctx) => {
+    sessionStorage.setItem("is-authenticated", "true");
+    return res(ctx.status(200));
+  }),
+  rest.get("/user", (req, res, ctx) => {
+    const isAuthenticated = sessionStorage.getItem("is-authenticated");
+    if (!isAuthenticated) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: "Not authorized",
+        })
+      );
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({
+        nickName: "신영",
+        id: "test@co.kr",
+      })
+    );
+  }),
+];
